a8306154439b2a98cab470c6781ffc51e4a1e904
kunal05-g <gurjar05kunal@gmail.com>
2025-05-12 22:06:39 +0530

    first commit

---
+++ sections/Navbar.tsx
@@ -0,0 +1,16 @@
+const navLinks = [
+    {label: 'Home', link: '/'},
+    {label: 'About', link: '/about'},
+    {label: 'Features', link: '/features'},
+    {label: 'FAQ', link: '/faq'},
+]
+
+const Navbar = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Navbar;
